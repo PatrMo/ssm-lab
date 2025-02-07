774975a5edcf469467093300dd2169245956c847
PatrMo <molka.patrick@gmail.com>
2025-02-06 20:54:27 -0500

    Explorer changes

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Explorer.java
@@ -1,12 +1,8 @@
-package ca.mcmaster.se2aa4.mazerunner;
-
 public class Explorer {
     private Position position;
-    private Maze maze;
 
     public Explorer(Maze maze) {
-        this.maze = maze;
-        this.position = maze.getEntry();
+        this.position = new Position(maze.getEntry().getX(), maze.getEntry().getY(), Direction.EAST, maze);
     }
 
     public Position getPosition() {
@@ -14,7 +10,7 @@ public class Explorer {
     }
 
     public void moveForward() {
-        if (canMoveForward()) {
+        if (position.canMoveForward()) {
             position.moveForward();
         }
     }
@@ -25,17 +21,5 @@ public class Explorer {
 
     public void turnLeft() {
         position.turnLeft();
-    }
-
-    private boolean canMoveForward() {
-        int x = position.getX();
-        int y = position.getY();
-        switch (position.getDirection()) {
-            case NORTH: return maze.isValidMove(x - 1, y);
-            case EAST:  return maze.isValidMove(x, y + 1);
-            case SOUTH: return maze.isValidMove(x + 1, y);
-            case WEST:  return maze.isValidMove(x, y - 1);
-            default: return false;
-        }
     }
 }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Position.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Position.java
@@ -3,40 +3,43 @@ package ca.mcmaster.se2aa4.mazerunner;
 public class Position {
     private int x, y;
     private Direction direction;
+    private Maze maze;
 
-    public Position(int x, int y, Direction direction) {
+    public Position(int x, int y, Direction direction, Maze maze) {
         this.x = x;
         this.y = y;
         this.direction = direction;
+        this.maze = maze;
     }
 
-    // getters
-    public int getX() { 
-        return x; 
-    }
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public Direction getDirection() { return direction; }
 
-    public int getY() { 
-        return y; 
-    }
-    public Direction getDirection() { 
-        return direction; 
-    }
-
-    // setters
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
 
-    // logic
-    public void moveForward() {
+    public boolean canMoveForward() {
         switch (direction) {
-            case NORTH: y--; break;
-            case EAST: x++; break;
-            case SOUTH: y++; break;
-            case WEST: x--; break;
+            case NORTH: return maze.isValidMove(x - 1, y);
+            case EAST: return maze.isValidMove(x, y + 1);
+            case SOUTH: return maze.isValidMove(x + 1, y);
+            case WEST: return maze.isValidMove(x, y - 1);
+            default: return false;
         }
     }
 
+    public void moveForward() {
+        if (!canMoveForward()) return;
+        switch (direction) {
+            case NORTH: x--; break;
+            case EAST: y++; break;
+            case SOUTH: x++; break;
+            case WEST: y--; break;
+        }
+    }
+    //setters
     public void turnRight() {
         direction = direction.turnRight();
     }
